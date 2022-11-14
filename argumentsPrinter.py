55e3bc5bdf95a75506ee9def1d1973f541d1e0b9
SCL <76017102+SimClb@users.noreply.github.com>
2022-11-14 14:34:33 +0100

    we have our program, we have to find how translate pseudo code of var arugments

---
+++ argumentsPrinter.py
@@ -0,0 +1,9 @@
+# you have to show the arguments define by the user on prompt 
+import sys
+
+
+n = int(sys.argv[1]) # = $1 from scriptshell
+arguments = #all of arguments
+for argument in arguments: 
+    print(argument, '\n')
+    
