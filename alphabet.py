ae3b15edca708658aab213b926e1a40c18f6eb70
SCL <76017102+SimClb@users.noreply.github.com>
2022-11-14 13:46:39 +0100

    exo alphabet finished

---
+++ alphabet.py
@@ -0,0 +1,12 @@
+# faire un programme qui affiche l'alphabet en lettre minuscules suivi d'un retour à la ligne 
+
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+lettersStr = "" 
+
+# each boucle put the new letter into the initialize str variable "letterStr" 
+for letter in letters: 
+    lettersStr += letter
+
+print(lettersStr)
+
