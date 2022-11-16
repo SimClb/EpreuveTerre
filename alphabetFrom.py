51e32147da448e367393631406c3a340974db397
SCL <76017102+SimClb@users.noreply.github.com>
2022-11-16 18:06:15 +0100

    alphabetFrom finished

--- alphabetFrom.py
+++ alphabetFrom.py
@@ -4,8 +4,11 @@ import sys
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
            "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+#if argument is a number ==> bools true 
 isNumber = sys.argv[1].isnumeric()
 
+
+#check if argument is ok 
 if isNumber:
     print("Veuillez rentrer une lettre et non un chiffre en argument...")
     exit()
@@ -13,6 +16,14 @@ elif len(sys.argv[1]) > 1:
     print("Veuillez n'entrer qu'une seule lettre en argument...")
     exit()
 
+#take the index of the argument 
+fromIndex = letters.index(str(sys.argv[1]))
+newList = letters[fromIndex:]
+listToPrint = ""
 
-#fromIndex = letters.index(str(sys.argv))
-#print("Voici l'alphbat à partir de la lettre que vous avez demandé: \n", letters[fromIndex:])
+for letter in newList:
+    listToPrint += letter
+
+print(listToPrint)
+
+#finished 
